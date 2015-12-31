d81f638224e272725377473da53bbf1b18d5b614
Alan <xuzhang@xzcode.com>
2015-12-31 15:50:46 -0500

    12/31

--- Amazon/Intern/Code Questions/Linked List.cpp
+++ Amazon/Intern/Code Questions/Linked List.cpp
@@ -6,3 +6,5 @@ Linked List
 4.查环
 5.Leetcode 原题，swap nodes in a linked list in pairs
 6.reverse a linked list
+7.Intersection of two linked lists 解法，赛跑
+follow up链表变成双链表.检查之前的节点 如果有交叉那么从其中一条链遍历到交叉点的时候无法回到上一个节点
