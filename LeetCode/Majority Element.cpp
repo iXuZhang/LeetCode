c124fd889e502f20dbcc88563240e34d48675f13
Alan <xuzhang@xzcode.com>
2016-01-29 21:30:25 -0500

    1/29

--- LeetCode/Majority Element.cpp
+++ LeetCode/Majority Element.cpp
@@ -2,11 +2,11 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority;
+        int majority = 0;
         int count = 0;
         for(int num : nums){
             if(count == 0) majority = num;
-            if(num == majority) count++;
+            if(majority == num) count++;
             else count--;
         }
         return majority;
